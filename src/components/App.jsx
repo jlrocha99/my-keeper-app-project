4ec1053626800078dab47b75d287adac0bf8a843
JL Rocha <jl.rocha0799@gmail.com>
2024-06-20 20:50:32 -0300

    end components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import Header from "./Header"
 import Note from "./Note"
 import Footer from "./Footer"
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 
 function Footer() {
+    var date = new Date().getFullYear()
+
     return(
         <footer className="text-center absolute w-screen bottom-0 h-10">
-            <p className="text-slate-400">Copyright ⓒ 2024</p>
+            <p className="text-slate-300">Copyright ⓒ {date}</p>
         </footer>
     )
 }
